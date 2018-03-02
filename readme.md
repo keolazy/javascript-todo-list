e4f80997428dc07a0390e05a4a178f593cfec778
Nathan Keolasy <keolazy1@gmail.com>
2018-03-02 00:41:21 -0600

    ian added so many cool features

--- readme.md
+++ readme.md
@@ -12,7 +12,7 @@ Create a todo list app using HTML, JS, and the CSS file `todos.css` in this dire
 
 * A user can hit enter in the text input to add a new todo
 * A user can click on the "Add Todo" button to add a new todo
-* Make sure the app looks just like the following gif.  Use the CSS to figure out what element types, classnames, and id's you may need to use.
+* Make sure the app looks just like the following gif.  Use the CSS to figure out what element types, classnames, and id's you may need to <use class=""></use>
 
 ### Part Two
 
--- scripts.js
+++ scripts.js
@@ -1,41 +1,112 @@
+let todoList = document.getElementById('todo-list');
+let inputField = document.getElementsByTagName('input')[0];
+let theButton = document.getElementById('add_todo');
+let todoArray = [];
 
-// List functions needed
-// displayTodo() addTodo();
-// deleteTodo(); changeTodo();
-//
-let input = document.getElementById('input'); // refers to input form
-let liList = document.getElementById('ul'); // place where new Lis stack
+// wow fancy undo features
+let undoBlock = document.getElementById('undo-block');
+let undoText = document.getElementById('undo-text');
+let savedTask;
 
+function addToDo() {
+	// Get user input and push to internal data array
+	let newToDo = inputField.value;
+	todoArray.push(newToDo);
 
-document.getElementById('add_todo').onclick = function() {
-  let el = document.createElement('li'); // <li>
-  el.innerText = input.value;
-  input.value = '';
-  liList.appendChild(el);
+	// Initialize new li and add data-id
+	let newLi = `<li data-id="${todoArray.length}">
+	<input type="checkbox" class="done">
 
-  console.log(input.value);
+	<div class="task-wrapper">
+	<span class="task-span" id="task-${todoArray.length}">${newToDo}</span>
+	</div>
+
+	<div class="delete">X</div>
+	</li>`;
+
+	// Append Li to ToDo list
+	todoList.innerHTML += newLi;
+
+	// Clear user input field
+	inputField.value = '';
 }
 
-// This is my groups code
-// var input = document.getElementById('input');
-// var li = document.getElementById('ul')
-//
-// document.getElementById('add_todo').onclick = function() {
-//   let el = document.createElement('li');
-//   el.innerText = input.value;
-//   input.value = '';
-//   li[0].appendChild(el);
-//
-//   console.log(input.value)
-// }
+function editTask(el) {
+	let newInput = `<input class="task-edit" type="textarea" id=${el.id} value="${
+		el.textContent
+	}">`;
+	el.parentNode.innerHTML = newInput;
+}
+
+function commitEdit(el) {
+	let newSpan = `<span class="task-span" id=${el.id}>${el.value}</span>`;
+	el.parentNode.innerHTML = newSpan;
+}
+
+// Not sure why this is needed
+function markDone(el) {
+	let taskSpan = el.parentElement.querySelector('span');
+	if (taskSpan.className === 'task-done') {
+		taskSpan.className = 'task-span';
+	} else {
+		taskSpan.className = 'task-done';
+	}
+}
+// classifies if task is done.
 
 
-// my attempt to edit their code
-// document.getElementById('add_todo').onclick = function() {
-//   let el = document.createElement('li'); // <li>
-//   el.innerText = input.value;
-//   input.value = '';
-//   liList[0].appendChild(el);
-//
-//   console.log(input.value);
-// }
+
+function deleteTask(el) {
+	el.parentNode.remove();
+}
+
+
+// Ian added this fancy Undo feature
+function enableUndo(el) {
+	savedTask = el.parentNode;
+	let savedText = savedTask.querySelector('span').textContent;
+	undoBlock.style.opacity = 1;
+	undoText.textContent = `You deleted '${savedText}'`;
+}
+
+function restoreTask() {
+	todoList.appendChild(savedTask);
+	undoBlock.style.opacity = 0;
+}
+
+todoList.addEventListener('dblclick', ev => {
+	if (ev.target.className == 'task-span') {
+		editTask(ev.target);
+	}
+});
+
+// Add event listeners
+theButton.addEventListener('click', () => {
+	if (inputField.value) {
+		addToDo();
+	}
+});
+
+todoList.addEventListener('click', ev => {
+	if (ev.target.className === 'done') {
+		markDone(ev.target);
+	} else if (ev.target.className === 'delete') {
+		enableUndo(ev.target);
+		deleteTask(ev.target);
+	}
+});
+
+document.addEventListener('keypress', ev => {
+	if (
+		ev.target.className === 'task-edit' &&
+		(ev.keycode === 13 || ev.which === 13)
+	) {
+		commitEdit(ev.target);
+	} else if (
+		(ev.keycode === 13 || ev.which === 13) &&
+		inputField.value &&
+		ev.target === inputField
+	) {
+		addToDo();
+	}
+});
